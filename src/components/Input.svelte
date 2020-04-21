410c1a93b4a6780c805fda38cf1b33b3c4c58777
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-04-21 18:50:21 -0300

    create nested component

---
+++ src/components/Input.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let value;
+</script>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    border: 2px dotted #ccc;
+    background: yellow;
+    padding: 10px;
+  }
+  input {
+    border: 2px solid #ccc;
+    border-radius: 5px;
+  }
+  span {
+    font-style: oblique;
+  }
+</style>
+
+<main>
+  <input type="text" bind:value />
+  <br />
+  <span>{value}</span>
+</main>
